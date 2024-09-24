f3330988da764016f10390bd01b2f85228f6c765
Sergey Mozhnov <s.mozhnov.v@gmail.com>
2024-09-24 19:09:45 +0300

    2commit

---
+++ 3_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
+	totalCount := 4
+	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	// здесь нужно добавить необходимые проверки
+	body := responseRecorder.Body.String()
+	list := strings.Split(body, ",")
+	assert.NotEqual(t, len(list), totalCount)
+
+}
